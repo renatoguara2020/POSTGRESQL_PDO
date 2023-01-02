fdd68ca05086f69704153eb0619f293c2b110db3
Renato Alves Soares <renatoguara2020@gmail.com>
2023-01-02 09:41:50 -0300

    ok

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+ 
+$host='localhost';
+$db = 'database_postgres';
+$username = 'postgres';
+$password = '456ALVES';
+ 
+$dsn = "pgsql:host=$host;port=5432;dbname=$db;user=$username;password=$password";
+ 
+try{
+ // create a PostgreSQL database connection
+ $conn = new PDO($dsn);
+ 
+ // display a message if connected to the PostgreSQL successfully
+ if($conn){
+    echo "Connected to the <strong>$db</strong> database Successfully!";
+ }
+} catch (PDOException $e){
+ // report error message
+ echo $e->getMessage();
+}
+ 
