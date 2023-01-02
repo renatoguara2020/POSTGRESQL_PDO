befa4c674e6ba9473a13ebe570885f82a1063eb7
Renato Alves Soares <renatoguara2020@gmail.com>
2023-01-02 10:24:01 -0300

    ok

--- index.php
+++ index.php
@@ -8,15 +8,41 @@ $password = '456ALVES';
 $dsn = "pgsql:host=$host;port=5432;dbname=$db;user=$username;password=$password";
  
 try{
+   $firstname = 'RENATO ALVES';
+   $lastname = 'RENATO ALVES';
+   $email = 'RENATO ALVES';
  // create a PostgreSQL database connection
  $conn = new PDO($dsn);
  
  // display a message if connected to the PostgreSQL successfully
  if($conn){
-    echo "Connected to the <ul>$db</ul> database Successfully!";
+    
+    $sql = ("INSERT INTO testes (firstname, lastname, email) VALUES (:firstname, :lastname, :email)");
+
+$stmt = $conn->prepare($sql);
+$stmt->bindValue(':firstname', $firstname, PDO::PARAM_STR);
+$stmt->bindValue(':lastname', $lastname, PDO::PARAM_STR);
+$stmt->bindValue(':email', $email, PDO::PARAM_STR);
+
+$stmt->execute();
+
+echo "Connected to the <strong>$db</strong> database Successfully!";
  }
 } catch (PDOException $e){
  // report error message
  echo $e->getMessage();
 }
  
+
+/ prepare statement for insert
+$sql = 'INSERT INTO tblemployee (employeenumber,firstname,lastname,email) VALUES (:employeenumber,:firstname,:lastname,:email)';
+$stmt = $pdo->prepare($sql);
+
+// pass values to the statement
+$stmt->bindValue(':employeenumber', $employeenumber);
+$stmt->bindValue(':firstname', $firstname);
+$stmt->bindValue(':lastname', $lastname);
+$stmt->bindValue(':email', $email);
+
+// execute the insert statement
+$stmt->execute();
