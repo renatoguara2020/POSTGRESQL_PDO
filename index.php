7c7c8f32bc7b7498b5d84de8bf68c18f152378d1
Renato Alves Soares <renatoguara2020@gmail.com>
2023-01-02 21:02:24 -0300

    ok

--- index.php
+++ index.php
@@ -1,123 +1,21 @@
 <?php
+$servername = "localhost";
+$username = "postgres";
+$password = "456ALVES";
+$dbname = "database_postgres";
 
-
-class DB {
-
-    private $pdo;
-
-    function __construct() {
-        require_once 'config.php';
-        try {
-            $connData = "pgsql:host=$host;port=5432;dbname=$dbName;";
-        
-            $this->pdo = new PDO($connData, $user, $password, [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);     
-        }
-        catch(PDOException $e) {
-            die($e->getMessage());
-        }
-    }
-
-    function begin() {
-        try {
-            $this->pdo->beginTransaction();
-        }
-        catch(PDOException $e) {
-            die($e->getMessage());
-        }
-    }
-
-    function commit() {
-        try {
-            $this->pdo->commit();
-        }
-        catch(PDOException $e) {
-            die($e->getMessage());
-        }
-    }
-
-    function rollback() {
-        try {
-            $this->pdo->rollBack();
-        }
-        catch(PDOException $e) {
-            die($e->getMessage());
-        }
-    }
-
-    // query method
-    // returns pdo statement object
-    function query(string $queryString, Array $substitutionArray) {
-        try {
-            $sql = $this->pdo->prepare($queryString);
-            $sql->execute($substitutionArray);
-            
-        }
-        catch(PDOException $e) {
-            die($e->getMessage());
-        }
-        return $sql;
-    }
-
-    // Returns a single row from the query parameter as an array
-    function fetchSingleRow(PDOStatement $sql) {
-        try {
-            $result = $sql->fetch(PDO::FETCH_ASSOC);
-            return $result;
-        }
-        catch(PDOException $e) {
-            die($e->getMessage());
-        }
-    }
-
-    // Returns all rows from the query parameter as an array of arrays
-    function fetchAll(PDOStatement $sql) {
-        try {
-            $result = $sql->fetchAll(PDO::FETCH_ASSOC);
-            return $result;
-        }
-        catch(PDOException $e) {
-            die($e->getMessage());
-        }
-    }
-
-    // Returns a specific number of rows specified in the parameters
-    function fetchRowNumber(PDOStatement $sql,integer $rowNumber) {
-        try {
-            $result = array();
-            for($i = 0; $i < $rowNumber; $i++) {
-                $tmp = $sql->fetch(PDO::FETCH_ASSOC);
-                array_push($result, $tmp);
-            }
-        }
-        catch(PDOException $e) {
-            die($e->getMessage());
-        }
-    }
-
-    // returns the number of rows resulting from the input query
-    function numRows(PDOStatement $sql) {
-        try {
-            return $sql->rowCount();
-        }
-        catch(PDOException $e) {
-            die($e->getMessage());
-        }
-    }
-
-    function insertRow($queryString, $substitutionArray) {
-        try {
-            $sql = $this->query($queryString, $substitutionArray);
-            if($sql && $this->numRows($sql) == 1) {
-                return 1;
-            }
-            else 
-                return -1;
-        }
-        catch(PDOException $e) {
-            die($e->getMessage());
-        }
-    }
+try {
+  $conn = new PDO("pgsql:host=$servername;dbname=$dbname", $username, $password);
+  // set the PDO error mode to exception
+  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  $stmt = $conn->prepare("INSERT INTO testes (firstname, lastname, email)
+  VALUES ('John', 'Doe', 'john@example.com')");
+  // use exec() because no results are returned
+  $stmt->execute();
+  echo "New record created successfully";
+} catch(PDOException $e) {
+  echo   $e->getMessage();
 }
 
-
+$conn = null;
 ?>
